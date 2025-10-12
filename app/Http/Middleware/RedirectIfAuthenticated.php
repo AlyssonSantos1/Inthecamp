f406523491e2998e8e1da474f7259f5e3ab2eb51
AlyssonSantos1 <salysson280@gmail.com>
2025-10-11 21:53:36 -0300

    update auth in progress

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -16,15 +16,33 @@ class RedirectIfAuthenticated
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     public function handle(Request $request, Closure $next, string ...$guards): Response
-    {
+   {
+        if ($request->is('login') || $request->is('login*')) {
+            return $next($request); 
+        }
+    
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                if (session()->has('user')) {
+                    $user = session('user');
+
+                switch (strtolower(trim($user))) {
+                    case 'executive':
+                        return redirect()->route('attendant.index');
+                    case 'manager':
+                        return redirect()->route('inventory.index');
+                    case 'internaladvisor':
+                        return redirect()->route('sommelier.index');
+                    default:
+                        return redirect('/');
+                }
+            } else {
+                return redirect('/');        
             }
         }
-
+    }
         return $next($request);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AttendantController;
+use App\Http\Controllers\InventoryController;
+use App\Http\Controllers\SommelierController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Auth\Middleware\Authenticate;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,14 +22,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
+Route::post('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
 
+Route::middleware('auth', 'check')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+//
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+//
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+//
+
+
 
 require __DIR__.'/auth.php';
