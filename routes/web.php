94ccea01fae59277d55a72ec03b42dc8d74e5073
AlyssonSantos1 <salysson280@gmail.com>
2025-10-26 22:07:33 -0300

    updates on routes

--- routes/web.php
+++ routes/web.php
@@ -27,18 +27,28 @@ Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('l
 Route::post('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
 
 Route::middleware(['auth', 'can:inventory'])->group(function(){
-    Route::get('/', [InventoryController::class, 'dashboard']);
-    Route::post('/', [InventoryController::class, 'dashboard']);
+    Route::get('/newstock', [InventoryController::class, 'newitem']);
+    Route::post('/newwine', [InventoryController::class, 'store'])->named('created');
+    Route::get('/changing/{id}', [InventoryController::class, 'deposit']);
+    Route::put('/changed/{id}', [InventoryController::class, 'max'])->named('changed');
+    Route::get('/changing/{id}', [InventoryController::class, 'garbage']);
+    Route::post('/changed/{id}', [InventoryController::class, 'scrap'])->named('deleted');
 });
 //
 
 Route::middleware(['auth', 'can:sommelier'])->group(function(){
-    Route::get('/', [SommelierController::class, 'dashboard']);
+    Route::get('/creating', [SommelierController::class, 'oenophile']);
+    Route::post('/newstock', [SommelierController::class, 'regulation']);
+    Route::get('/newwine/{id}', [SommelierController::class, 'blend']);
+    Route::put('/changinge/{id}', [SommelierController::class, 'vintage']);
 });
 //
 
 Route::middleware(['auth', 'can:attendant'])->group(function(){
-    Route::get('/', [AttendantController::class, 'dashboard']);
+    Route::get('/creating', [AttendantController::class, 'create']);
+    Route::post('/newstock', [AttendantController::class, 'store']);
+    Route::get('/order/{id}', [AttendantController::class, 'booking']);
+    Route::put('/changed/{id}', [AttendantController::class, 'transaction']);
 });
     
 
